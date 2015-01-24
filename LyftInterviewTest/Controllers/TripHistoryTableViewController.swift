68d37873a97eaf441a0d4a4e81424c4544b26069
John Tumminaro <john@tinylittlegears.com>
2015-01-23 17:25:33 -0800

    Assets added, fetched results controller wired up, started styling appearance.

--- LyftInterviewTest/Controllers/TripHistoryTableViewController.swift
+++ LyftInterviewTest/Controllers/TripHistoryTableViewController.swift
@@ -8,16 +8,18 @@
 
 import UIKit
 
-class TripHistoryTableViewController: UITableViewController {
+class TripHistoryTableViewController: UITableViewController, NSFetchedResultsControllerDelegate {
+    
+    lazy var fetchedResultsController = Trip.MR_fetchAllSortedBy("startTime",
+        ascending: false,
+        withPredicate: nil,
+        groupBy: nil,
+        delegate: nil)
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem()
+        fetchedResultsController.delegate = self
+        tableView.reloadData()
     }
 
     override func didReceiveMemoryWarning() {
@@ -28,70 +30,75 @@ class TripHistoryTableViewController: UITableViewController {
     // MARK: - Table view data source
 
     override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
-        // #warning Potentially incomplete method implementation.
-        // Return the number of sections.
-        return 0
+        return 1
     }
 
     override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete method implementation.
-        // Return the number of rows in the section.
+        if let fetchedObjects = fetchedResultsController.fetchedObjects {
+            return fetchedObjects.count
+        }
         return 0
     }
-
-    /*
+    
     override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCellWithIdentifier("reuseIdentifier", forIndexPath: indexPath) as UITableViewCell
+        let cell = tableView.dequeueReusableCellWithIdentifier("tripCell", forIndexPath: indexPath) as UITableViewCell
 
         // Configure the cell...
 
         return cell
     }
-    */
-
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(tableView: UITableView, canEditRowAtIndexPath indexPath: NSIndexPath) -> Bool {
-        // Return NO if you do not want the specified item to be editable.
-        return true
+    
+    // MARK: Fetched results controller delegate
+    
+    func controllerWillChangeContent(controller: NSFetchedResultsController) {
+        tableView.beginUpdates()
     }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
-        if editingStyle == .Delete {
-            // Delete the row from the data source
-            tableView.deleteRowsAtIndexPaths([indexPath], withRowAnimation: .Fade)
-        } else if editingStyle == .Insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
+    
+    func controller(controller: NSFetchedResultsController, didChangeSection sectionInfo: NSFetchedResultsSectionInfo, atIndex sectionIndex: Int, forChangeType type: NSFetchedResultsChangeType) {
+        if type == .Insert {
+            tableView.insertSections(NSIndexSet(index: sectionIndex), withRowAnimation: .Fade)
+        } else if type == .Delete {
+            tableView.deleteSections(NSIndexSet(index: sectionIndex), withRowAnimation: .Fade)
+        }
     }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(tableView: UITableView, moveRowAtIndexPath fromIndexPath: NSIndexPath, toIndexPath: NSIndexPath) {
-
+    
+    func controller(controller: NSFetchedResultsController, didChangeObject anObject: AnyObject, atIndexPath indexPath: NSIndexPath?, forChangeType type: NSFetchedResultsChangeType, newIndexPath: NSIndexPath?) {
+        switch type {
+        case .Insert:
+            if let newPath = newIndexPath {
+                tableView.insertRowsAtIndexPaths([newPath], withRowAnimation: .Fade)
+            }
+        case .Delete:
+            if let idxPath = indexPath {
+                tableView.deleteRowsAtIndexPaths([idxPath], withRowAnimation: .Fade)
+            }
+        case .Update:
+            if let idxPath = indexPath {
+                if let cell = tableView.cellForRowAtIndexPath(idxPath) {
+                    configureCell(cell, atIndexPath: idxPath)
+                }
+            }
+        case .Move:
+            if let newPath = newIndexPath {
+                if let idxPath = indexPath {
+                    tableView.deleteRowsAtIndexPaths([idxPath], withRowAnimation: .Fade)
+                    tableView.insertRowsAtIndexPaths([newPath], withRowAnimation: .Fade)
+                }
+            }
+        }
     }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(tableView: UITableView, canMoveRowAtIndexPath indexPath: NSIndexPath) -> Bool {
-        // Return NO if you do not want the item to be re-orderable.
-        return true
+    
+    func controllerDidChangeContent(controller: NSFetchedResultsController) {
+        tableView.endUpdates()
     }
-    */
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using [segue destinationViewController].
-        // Pass the selected object to the new view controller.
+    // MARK: - Internal
+    
+    private func configureCell(cell: UITableViewCell, atIndexPath: NSIndexPath) {
+        
     }
-    */
-
+    
+    
+    
+    
 }
---
+++ LyftInterviewTest/Utilities/Appearance.swift
@@ -0,0 +1,16 @@
+//
+//  Appearance.swift
+//  LyftInterviewTest
+//
+//  Created by John Tumminaro on 1/23/15.
+//  Copyright (c) 2015 Tiny Little Gears. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class Appearance {
+    class func setupStyles() {
+        UINavigationBar.appearance().barTintColor = UIColor(red: 252/255, green: 251/255, blue: 248/255, alpha: 1)
+    }
+}
