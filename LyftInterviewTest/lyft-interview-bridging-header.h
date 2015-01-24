48d233fd5acaea018d715b08c3242bb8cb559dc4
John Tumminaro <john@tinylittlegears.com>
2015-01-23 16:28:39 -0800

    Added bridging header.

---
+++ LyftInterviewTest/lyft-interview-bridging-header.h
@@ -0,0 +1,10 @@
+//
+//  lyft-interview-bridging-header.h
+//  LyftInterviewTest
+//
+//  Created by John Tumminaro on 1/23/15.
+//  Copyright (c) 2015 Tiny Little Gears. All rights reserved.
+//
+
+
+@import MagicalRecord;
